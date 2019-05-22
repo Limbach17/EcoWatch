317b49af36f3176f721bb802950a95fa3035ccd4
glory2hypnotoad <matthew.limbach1@icloud.com>
2019-05-22 00:58:02 -0400

    Index.handlebars created. Background image saved. Html routes for index and 404 added.

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+var path = require("path");
+var db = require("../models");
+
+module.exports = function(app) {
+
+  app.get("/", function(req, res) {
+    res.render("index", {});
+  });
+
+  
+  app.get("*", function(req, res) {
+    res.render("404", {});
+  });
+
+};
